f614ad0163f3048e73e8187c304c652b266cec6d
Jude MacDonald <judemacdonald6@gmail.com>
2016-09-18 18:04:05 -0600

    Doing a random/mouse bound ball.

---
+++ 191-p5/Randomized Balls/sketch.js
@@ -0,0 +1,29 @@
+var x = 197.5
+var y = 175
+
+function preLoad() {
+  img = loadImage(bugs.gif);
+}
+
+function setup() {
+  createCanvas(395, 350);
+ //frameRate(10);
+}
+
+function draw() {
+  fill(mouseX, 0, 0);
+  background(mouseY, mouseX, mouseY, 25);
+  noStroke();
+  ellipse(width/2, height/2, mouseX, mouseY);
+
+  var g = random(255)  
+  fill(0, 0, g)
+  ellipse(x, y, 50, 50);
+  x = x + random(-1, 1);
+  y = y + random(-1, 1);
+  
+  var xWidth = random(width)
+  var yHeight = random(width)
+  fill(0, g, 0)
+  ellipse(xWidth, yHeight, 50, 50)
+}
