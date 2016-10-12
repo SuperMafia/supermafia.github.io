2253569a3ff838835ec8968b0aaf800166ad9e67
Jude MacDonald <judemacdonald6@gmail.com>
2016-10-11 20:29:57 -0600

    Laser beams!

--- 191-p5/Class Objects/square.js
+++ 191-p5/Class Objects/square.js
@@ -1,11 +1,12 @@
+//Defined all the parameters for my Diamonds. It used to be squares, though it morphed into Diamonds because Steven Universe references and diamond shapes. Includes base speed and position.
 function Squares(){
-  this.posX = 50;
-  this.posY = 50;
-  this.speedX = 2;
+  this.posX = 1; //PosX and PosY has every diamond placed inside one another.
+  this.posY = 1;
+  this.speedX = 1;
   this.speedY = 2;
-  this.dirX = 1;
+  this.dirX = 1; //All diamonds move down and right at a speed of 2.
   this.dirY = 1;
-  this.r = random(225,255);
+  this.r = random(225,255); //Allows me to control the colors to just be variants of pink.
   this.g = random(0, 113);
   this.b = random(153, 182);
 }
@@ -28,7 +29,7 @@ Squares.prototype.moveSquare = function() {
   this.posY = this.posY + (this.dirY*this.speedY);
   if(this.posX >= width || this.posX <= 0){
     this.dirX = -1*this.dirX;
-    this.speedX = random(5,25);
+    this.speedX = random(5,20); //Sets speed in between 5 up to 25.
     if(this.posX >= width){
       this.posX = width - 1;
     }
@@ -38,7 +39,7 @@ Squares.prototype.moveSquare = function() {
   }
   if(this.posY >= height || this.posY <= 0){
     this.dirY = -1*this.dirY;
-    this.speedY = random(5,10);
+    this.speedY = random(5, 15); //Sets speed to be 5 up to 15 or any floats in between.
     if(this.posY >= height){
       this.posY = height - 1;
     }
