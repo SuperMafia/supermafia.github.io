145148c499322fe5ca8f13af1dca5ec064d97167
Jude MacDonald <judemacdonald6@gmail.com>
2016-11-01 22:37:37 -0600

    Tried Again with JSON
    Forgot that I need semicolons instead of a comma for the "for"
    statements.

--- 191-p5/JSON/sketch.js
+++ 191-p5/JSON/sketch.js
@@ -1,5 +1,3 @@
-// www.giantbomb.com/api/game/3030-49139/?api_key=6b6f1eef5d5ddc08447936197965d53e210c2074&format=json&field_list=genres,name
-// www.giantbomb.com/api/reviews/?api_key=6b6f1eef5d5ddc08447936197965d53e210c2074&format=json&limit=10&field_list=game,score,site_detail_url
 var x = 50
 var y = 150
 
@@ -16,14 +14,14 @@ function draw(){
 
 function gameReview(game){
   console.log(game);
-  var name = game.main.name;
-  console.log(name);
-  var score = game.main.score;
+  var name = game.main.name; //Attempt at getting the name of the game at review.
+  console.log(name); //And placing it in the Console Log.
+  var score = game.main.score; //Doing the same thing, except for the score itself.
   console.log(score);
 
   background(0);
   fill(0, 255, 0, 50);
-  for(var i = 0; i < 10, i++){
-    ellipse(i*x+50, y, 50, 50);
+  for(var i = 0; i < 10; i++){
+    ellipse(i*x+50, y, 50/score, 50/score);
   }
 }
