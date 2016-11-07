716a18f5e6be1ebb13cd973e5c6606883fe8a05b
Jude MacDonald <judemacdonald6@gmail.com>
2016-11-06 20:15:16 -0700

    json

--- 191-p5/JSON/sketch.js
+++ 191-p5/JSON/sketch.js
@@ -1,27 +1,23 @@
-var x = 50
-var y = 150
+// var x = 50
+// var y = 150
+var gameReview
+
+function preload(){
+  gameReview = loadJSON("giantbomb.json")
+}
 
 function setup(){
-  var url = "http://www.giantbomb.com/api/reviews/?api_key=6b6f1eef5d5ddc08447936197965d53e210c2074&format=json&limit=10&field_list=game,score,site_detail_url"
-  loadJSON(url, gameReview);
-  createCanvas(750, 250);
-  noLoop();
-  noStroke();
+  noCanvas();
+  var review = getReview(gameReview);
+  createP("This game has been reviewed" + review + "/5 Stars.");
 }
 
-function draw(){
+function getReview(results){
+  var results = data.results;
+  var item = results[1];
+  var game = item.game;
+  var score = game.score;
+  return score;
 }
 
-function gameReview(game){
-  console.log(game);
-  var name = game.main.name; //Attempt at getting the name of the game at review.
-  console.log(name); //And placing it in the Console Log.
-  var score = game.main.score; //Doing the same thing, except for the score itself.
-  console.log(score);
-
-  background(0);
-  fill(0, 255, 0, 50);
-  for(var i = 0; i < 10; i++){
-    ellipse(i*x+50, y, 50/score, 50/score);
-  }
-}
+// Originated from: http://www.giantbomb.com/api/reviews/?api_key=6b6f1eef5d5ddc08447936197965d53e210c2074&format=json&limit=10&field_list=game,score,site_detail_url
